845e3193324fae1419386ddd9615e3e1be7253a6
kimyouwon <qudrhlfl3@gmail.com>
2024-05-28 23:09:52 +0900

    회원가입 수정

--- src/main/java/edu/yonsei/project/repository/UserRepository.java
+++ src/main/java/edu/yonsei/project/repository/UserRepository.java
@@ -7,6 +7,8 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository <UserEntity, Long> {
     Optional<UserEntity> findByLoginId(String loginId);
+    boolean existsByLoginId(String loginId);
+    boolean existsByNickname(String nickname);
 }
 
 
