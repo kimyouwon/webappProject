ebccb2643e623adc78d7baa318a2a5083d8bae2e
kimyouwon <qudrhlfl3@gmail.com>
2024-05-29 14:16:42 +0900

    메인페이지에서 최신 리뷰 3개 보임

--- src/main/java/edu/yonsei/project/service/ReviewService.java
+++ src/main/java/edu/yonsei/project/service/ReviewService.java
@@ -13,13 +13,11 @@ import java.util.List;
 @Service
 public class ReviewService {
 
-    // 여기에 Repository를 주입받아 사용합니다.
     @Autowired
     private ReviewRepository reviewRepository;
 
     @Autowired
     private LikeRepository likeRepository;
-
 
     public List<ReviewEntity> getAllReviews() {
         return reviewRepository.findAll();
@@ -28,8 +26,13 @@ public class ReviewService {
     public ReviewEntity getReviewById(Long id) {
         return reviewRepository.findById(id).orElseThrow(() -> new RuntimeException("리뷰를 찾을 수 없습니다."));
     }
+
     public List<ReviewEntity> findByUserId(String userId) {
         return reviewRepository.findByUserId(userId);
+    }
+
+    public List<ReviewEntity> getRecentReviews() {
+        return reviewRepository.findTop3ByOrderByIdDesc();
     }
 
     public void updateReview(Long id, String content) {
@@ -37,12 +40,13 @@ public class ReviewService {
         review.setContent(content);
         reviewRepository.save(review);
     }
+
     public ReviewEntity saveReview(ReviewEntity review) {
         return reviewRepository.save(review);
     }
+
     @Transactional
     public void deleteReview(Long id) {
-        // 관련된 likes 레코드 삭제
         ReviewEntity review = reviewRepository.findById(id).orElse(null);
         if (review != null) {
             List<LikeEntity> likes = likeRepository.findByReview(review);
